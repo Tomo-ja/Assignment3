7d83d71f44c054108b00a133a7d78a000df41ae4
Tomo-ja <tomonao.hj@gmail.com>
2022-04-28 20:21:51 -0700

    create story board

---
+++ Assignment3/LibraryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryViewController.swift
+//  Assignment3
+//
+//  Created by Tomonao Hashiguchi on 2022-04-28.
+//
+
+import Foundation
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
